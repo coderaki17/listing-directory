4d0804232e87e30a02dfb04b470816c93e5f344b
coderaki17 <94616218+coderaki17@users.noreply.github.com>
2021-12-24 18:15:03 +0530

    completed the task with given instructions
    listed directory using input from user and used subprocess module only , Tests ran successfully.

--- string_dir.py
+++ string_dir.py
@@ -1,6 +1,22 @@
 import subprocess
 
- 
-p = subprocess.run(['ls', '-la'])
+def return_list(directory):
+	sp = subprocess.Popen(['ls', '-l', directory],shell=True,stdout=subprocess.PIPE)
+	stdout,stderr= sp.communicate()
 
-print(p.returncode)
+
+	directory_list=[x.split(' ')[-1] for x in stdout.decode().split('\n')[1:-1]]
+	# num=directory_list.index('..\r')
+	# directory_list=directory_list[num+1:-3]
+	# directory_list=[i.replace('\r','')for i in directory_list]
+	
+	return directory_list
+	
+	
+
+directory=input("Enter the directory :")
+directory_list=return_list(directory)
+
+print(directory_list)
+
+
--- test_string_dir.py
+++ test_string_dir.py
@@ -2,10 +2,28 @@ import unittest
 from string_dir import *
 
 
-class TestStringDir(unittest.TestCase):
+class Testing(unittest.TestCase):
+    
+    def test_running(self):
+        self.assertTrue(return_list(directory))
 
-    def testing(self):
-        self.assertTrue((p()))
+    def test_output_is_list(self):
+        self.assertTrue(isinstance(return_list(directory),list))  
+
+    def test_content_check(self):
+        if "string_dir.py" in return_list(directory):
+            self.assertTrue(True)
+
+        
+if __name__ == "__main__":
+    unittest.main()
+
+    
+
+              
+        
+
+
 
     
         
