9a117073da77b87c9ace523b0bf48aa6ada2cd87
coderaki17 <94616218+coderaki17@users.noreply.github.com>
2022-01-05 17:51:47 +0530

    passed directory name in test cases

--- test_string_dir.py
+++ test_string_dir.py
@@ -5,13 +5,13 @@ from string_dir import *
 class Testing(unittest.TestCase):
     
     def test_running(self):
-        self.assertTrue(return_list(directory))
+        self.assertTrue(return_list('/Users/akashrc/pytest'))
 
     def test_output_is_list(self):
-        self.assertTrue(isinstance(return_list(directory),list))  
+        self.assertTrue(isinstance(return_list('/Users/akashrc/pytest'),list)) 
 
     def test_content_check(self):
-        if "string_dir.py" in return_list(directory):
+        if "string_dir.py" in return_list('/Users/akashrc/pytest'):
             self.assertTrue(True)
 
         
